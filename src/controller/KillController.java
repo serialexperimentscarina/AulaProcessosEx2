398df4af32b5669103a1b3197ba2f6ca0dfb10cc
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-02-26 18:29:10 -0300

    Create "listaProcessos" method

--- src/controller/KillController.java
+++ src/controller/KillController.java
@@ -1,5 +1,9 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class KillController {
 	
 	public KillController() {
@@ -10,6 +14,27 @@ public class KillController {
 	private String os() {
 		String os = System.getProperty("os.name");
 		return os;
+	}
+	
+	// Verifica o SO e, de acordo com SO, seleciona o comando para listar os processos ativos.
+	public void listaProcessos() {
+		String process = (os().contains("Windows")) ? "tasklist /fo table" : "ps -ef";
+		try {
+			Process p = Runtime.getRuntime().exec(process);
+			InputStream fluxo = p.getInputStream();
+			InputStreamReader leitor = new InputStreamReader(fluxo);
+			BufferedReader buffer = new BufferedReader(leitor);
+			String linha = buffer.readLine();
+			while (linha != null) {
+				System.out.println(linha);
+				linha = buffer.readLine();
+			}
+			buffer.close();
+			leitor.close();
+			fluxo.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,9 +1,13 @@
 package view;
 
+import controller.KillController;
+
 public class Main {
 
 	public static void main(String[] args) {
 
+		KillController killController = new KillController();
+		
 	}
 
 }
