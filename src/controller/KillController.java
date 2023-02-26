3447bc396deb2bc6f5b0ba88f06045e93d1adf46
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-02-26 18:12:28 -0300

    Create "os" method

--- src/controller/KillController.java
+++ src/controller/KillController.java
@@ -5,5 +5,11 @@ public class KillController {
 	public KillController() {
 		super();
 	}
+	
+	// Identifica e retorna o nome do Sistema Operacional
+	private String os() {
+		String os = System.getProperty("os.name");
+		return os;
+	}
 
 }
