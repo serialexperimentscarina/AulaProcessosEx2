3641a7dcf1ba19f871def4192fe2a60233fab8ec
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-02-26 23:09:55 -0300

    Change opc to String

--- src/view/Main.java
+++ src/view/Main.java
@@ -7,32 +7,31 @@ import controller.KillController;
 public class Main {
 
 	public static void main(String[] args) {
-		int opc;
+		String opc = "";
 		String processo;
 		KillController killController = new KillController();
 		Scanner input = new Scanner(System.in);
 		
 		do {
 			System.out.println("\nMenu : \n1 - Liste processos \n2- Matar um processo \n3 - Finalizar");
-			opc = input.nextInt();
+			opc = input.nextLine();
 			switch (opc) {
-				case 1:
+				case "1":
 					killController.listaProcessos();
 					break;
-				case 2:
+				case "2":
 					System.out.println("Digite o nome ou PID do processo que deseja encerrar: ");
-					input.nextLine();
 					processo = input.nextLine();
 					killController.mataProcesso(processo);
 					break;
-				case 3: 
+				case "3": 
 					System.out.println("Encerrando execução.");
 					break;
 				default: 
 					System.out.println("Opção inválida!");
 					break;
 			}
-		} while (opc != 3);
+		} while (!opc.equals("3"));
 		input.close();
 		
 	}
